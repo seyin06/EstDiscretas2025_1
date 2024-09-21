6c62d8edbf9ddebd760d0b9f081ee1c9f936c020
seyin06 <seyin06rv@ciencias.unam.mx>
2024-09-20 18:17:32 -0600

    subi la practica 2

---
+++ Practica2.hs
@@ -0,0 +1,37 @@
+longitud :: [a] -> Int
+longitud [] = 0
+longitud (x:xs) = 1 + longitud xs
+
+sumaLista :: Num a =>[a] -> a
+sumaLista [] = 0
+sumaLista (x:xs) = x + sumaLista xs
+
+agregaElemento :: [a] -> a -> Bool -> [a]
+agregaElemento [] elemento valor = [elemento]
+agregaElemento lista elemento valor =
+  if valor
+  then elemento : lista
+  else lista ++ [elemento]
+
+maximoLista :: (Num a, Ord a) => [a] -> a
+maximoLista [] = error "Lista está vacia"
+maximoLista (x:xs) =
+  if x > maximoLista xs
+  then x
+  else maximoLista xs
+
+indice :: [a] -> Int -> a
+indice (x:_) 0 = x
+indice (_:xs) n = indice xs (n-1)
+indice [] _ = error "El indice está fuera del rango"
+
+divisores :: Int -> [Int]
+divisores 0 = error "Valores infinitos"
+divisores n = [x | x <- [1..n],mod n x == 0]
+
+conjunto :: Eq a => [a] -> [a]
+conjunto [] = []
+conjunto (x:xs) = x : conjunto [p|p <- xs, p /= x]
+
+numerosPares :: [Int] -> [Int]
+numerosPares (x:xs)  = [p|p <- (x:xs), p `mod` 2 == 0]
