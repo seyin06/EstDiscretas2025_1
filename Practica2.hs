ce1cc71a3afbf995822e55c88e45f6d241de2140
seyin06 <seyin06rv@ciencias.unam.mx>
2024-11-06 08:13:16 -0600

    Practica2 corregida

--- Practica2.hs
+++ Practica2.hs
@@ -7,27 +7,24 @@ sumaLista [] = 0
 sumaLista (x:xs) = x + sumaLista xs
 
 agregaElemento :: [a] -> a -> Bool -> [a]
-agregaElemento lista elemento valor =
-  if valor
-  then elemento : lista
-  else lista ++ [elemento]
+agregaElemento lista elemento valor = if valor
+                                      then elemento : lista
+				      else lista ++ [elemento]
 
 maximoLista :: (Num a, Ord a) => [a] -> a
 maximoLista [] = error "La lista está vacía"
 maximoLista [x] = x
-maximoLista (x:xs) =
-  if x > maximoLista xs
-  then x
-  else maximoLista xs
+maximoLista (x:xs) = if x > maximoLista xs
+                      then x
+		      else maximoLista xs
 
 indice :: [a] -> Int -> a
 indice []_ = error "La lista está vacía"                              
-indice (_:xs) n =
-    if n > 0 && n <= longitud xs
-    then indice xs (n-1)
-    else if n == 0
-    then error "El número no es válido"
-    else error "Indice fuera de rango"
+indice (x:xs) n = if n > 0
+                   then error "Indice fuera de rango"
+		   else if n == 0
+		   then x
+		   else indice xs (n-1)
 
 divisores :: Int -> [Int]
 divisores n = [x | x <- [1..n],mod n x == 0]
