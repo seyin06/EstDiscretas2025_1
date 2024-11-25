0252f74557daba088ada0b91f938aae155573565
seyin06 <seyin06rv@ciencias.unam.mx>
2024-11-24 18:13:24 -0600

    corregí la identación de la Practica5

--- Practica5.hs
+++ Practica5.hs
@@ -48,25 +48,25 @@ equivalencia (formula1 :<=>: formula2) = equivalencia ((formula1 :&: formula2) :
 interpretacion :: Formula -> [(Var,Bool)] -> Bool
 interpretacion (Atom var) valores = valoresAux var valores
 interpretacion (Neg formula) valores = if interpretacion formula valores
-                                       then False
+                                            then False
                                             else True
 interpretacion (formula1 :&: formula2) valores = interpretacion formula1 valores && interpretacion formula2 valores
 interpretacion (formula1 :|: formula2) valores = interpretacion formula1 valores || interpretacion formula2 valores
 interpretacion (formula1 :=>: formula2) valores = if interpretacion formula1 valores
-                                                  then interpretacion formula2 valores
+                                                        then interpretacion formula2 valores
                                                         else True
 interpretacion (formula1 :<=>: formula2) valores = interpretacion formula1 valores == interpretacion formula2 valores
 
 valoresAux :: Var -> [(Var, Bool)] -> Bool
 valoresAux var [] = error "Una de las variables no esta definida"
 valoresAux var ((v,valor):xs) = if var == v
-                                then valor
+                                    then valor
                                     else valoresAux var xs
 
 
 combinaciones :: Formula -> [[(Var,Bool)]]
 combinaciones formula = if null (conjunto (variables formula))
-                        then [[]]
+                            then [[]]
                             else combinar (conjunto (variables formula))
 
 combinar :: [Var] -> [[(Var,Bool)]]
